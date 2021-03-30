c814685d47c7a61471c6954a8b28f5cbb96e70ef
Yong Cho <yong-cho@outlook.com>
2021-03-29 23:33:58 -0500

    completed node_calculator

--- node_calculator/app.js
+++ node_calculator/app.js
@@ -1,17 +1,22 @@
-// Your script must have:
-// A function that adds two numbers and returns the result
-// A function that multiplies two numbers and returns the result
-// A function that divides two numbers and returns the result
-// A function that subtracts two numbers and returns the result
-// Each function will have 2 parameters, num1, num2
-// On the console the prompt will print to the user:
-// Please enter your first number (store that number)
-// Please enter your second number (store that number)
-// Please enter the operation to perform: add, sub, mul, div (then store the operation)
-// Based on the operation entered by the user, you will call one of your 4 functions to perform the correct operation
-// You will then print to the console: The result is: [the result]
+var readlineSync = require('readline-sync');
 
-var readlineSync = require('readline-sync'),
-  animals = ['Lion', 'Elephant', 'Crocodile', 'Giraffe', 'Hippo'],
-  index = readlineSync.keyInSelect(animals, 'Which animal?');
-console.log('Ok, ' + animals[index] + ' goes to your room.')
+
+function calculate(num1, num2, operator) {
+    if (operator === 'add') {
+        return parseInt(num1, 16) + parseInt(num2, 16);
+    } else if (operator === 'sub') {
+        return num1 - num2;
+    } else if (operator === 'mul') {
+        return num1 * num2;
+    } else if (operator === 'div') {
+        return num1 / num2;
+    } else {
+        return "error";
+    }
+}
+
+let num1 = readlineSync.question('Please enter your first number: ')
+let num2 = readlineSync.question('Please enter your second number: ')
+let operator = readlineSync.question('Please enter the operation to perform: add, sub, mul, div: ')
+
+console.log("The result is: " + calculate(num1, num2, operator))
