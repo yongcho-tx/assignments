794dd39c8a9dd611bcf9c1508debf28831449a03
Yong Cho <yong-cho@outlook.com>
2021-03-29 19:08:32 -0500

    changed branch name to main

---
+++ node_calculator/app.js
@@ -0,0 +1,17 @@
+// Your script must have:
+// A function that adds two numbers and returns the result
+// A function that multiplies two numbers and returns the result
+// A function that divides two numbers and returns the result
+// A function that subtracts two numbers and returns the result
+// Each function will have 2 parameters, num1, num2
+// On the console the prompt will print to the user:
+// Please enter your first number (store that number)
+// Please enter your second number (store that number)
+// Please enter the operation to perform: add, sub, mul, div (then store the operation)
+// Based on the operation entered by the user, you will call one of your 4 functions to perform the correct operation
+// You will then print to the console: The result is: [the result]
+
+var readlineSync = require('readline-sync'),
+  animals = ['Lion', 'Elephant', 'Crocodile', 'Giraffe', 'Hippo'],
+  index = readlineSync.keyInSelect(animals, 'Which animal?');
+console.log('Ok, ' + animals[index] + ' goes to your room.')
