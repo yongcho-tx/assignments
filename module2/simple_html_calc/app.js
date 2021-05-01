834d6f35b1b8180ffc11c274a1f979534b0b6652
Yong Cho <yong-cho@outlook.com>
2021-04-30 22:57:18 -0500

    added simple calculator

---
+++ module2/simple_html_calc/app.js
@@ -0,0 +1,42 @@
+
+const addForm = document['add-form']
+
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    let number1 = Number(addForm.firstNumber.value)
+    let number2 = Number(addForm.secondNumber.value)
+   
+    sum = number1 + number2
+    const span = document.createElement('span')
+    span.textContent = sum
+    document.getElementById('addEquals').append(span)
+})
+
+const subForm = document['sub-form']
+
+subForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let number1 = Number(subForm.firstNumber.value)
+    let number2 = Number(subForm.secondNumber.value)
+
+    sub = number1 - number2
+    const span = document.createElement('span')
+    span.textContent = sub
+    document.getElementById('subEquals').append(span)
+})
+
+const multiForm = document['multi-form']
+
+multiForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    let number1 = Number(multiForm.firstNumber.value)
+    let number2 = Number(multiForm.secondNumber.value)
+
+    multi = number1 * number2
+    const span = document.createElement('span')
+    span.textContent = multi
+    document.getElementById('multiEquals').append(span)
+})
