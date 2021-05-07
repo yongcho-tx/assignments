4d765aed32116f8ea709f467810186e298f8027f
Yong Cho <yong-cho@outlook.com>
2021-05-06 22:02:10 -0500

    revised simple calc

--- module2/simple_html_calc/app.js
+++ module2/simple_html_calc/app.js
@@ -1,48 +1,42 @@
+const addForm = document['addform']
+const subForm = document['subform']
+const multiForm = document['multiform']
+const logAdd = document.querySelector('.logAdd')
+const logSub = document.querySelector('.logSub')
+const logMulti = document.querySelector('.logMulti')
 
-const addForm = document['add-form']
-
-addForm.addEventListener('submit', function(e) {
+const additionSubmit = (e) => {
     e.preventDefault()
-
     let number1 = Number(addForm.firstNumber.value)
     let number2 = Number(addForm.secondNumber.value)
-   
     sum = number1 + number2
-    const span = document.createElement('span')
-    span.textContent = ` ${sum}`
-    span.style.fontSize = '20px'
-    span.style.color = 'blue'
-    document.getElementById('addEquals').append(span)
-})
+    logAdd.textContent = sum
+}
+addForm.addEventListener('submit', additionSubmit)
 
-const subForm = document['sub-form']
 
-subForm.addEventListener('submit', (e) => {
+const subtractSubmit = (e) => {
     e.preventDefault()
-
     let number1 = Number(subForm.firstNumber.value)
     let number2 = Number(subForm.secondNumber.value)
 
     sub = number1 - number2
-    const span = document.createElement('span')
-    span.textContent = ` ${sub}`
-    span.style.color = 'red'
-    span.style.fontSize = '20px'
-    document.getElementById('subEquals').append(span)
-})
+    logSub.textContent = sub
+    subForm.reset()
+}
 
-const multiForm = document['multi-form']
+subForm.addEventListener('submit', subtractSubmit)
 
-multiForm.addEventListener('submit', (e) => {
+
+const multiplySubmit = (e) => {
     e.preventDefault()
 
     let number1 = Number(multiForm.firstNumber.value)
     let number2 = Number(multiForm.secondNumber.value)
 
     multi = number1 * number2
-    const span = document.createElement('span')
-    span.textContent = " " + multi
-    span.style.color = 'green'
-    span.style.fontSize = '20px'
-    document.getElementById('multiEquals').append(span)
-})
+    logMulti.textContent = " " + multi
+    multiForm.reset()
+}
+
+multiForm.addEventListener('submit', multiplySubmit)
