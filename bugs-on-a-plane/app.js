8ed56bc51f677b5a67cf0fa94c0a645c8eac834b
Yong Cho <yong-cho@outlook.com>
2021-05-07 06:57:25 -0500

    revised bugs on a plane

--- bugs-on-a-plane/app.js
+++ bugs-on-a-plane/app.js
@@ -10,18 +10,14 @@ function formAlert() {
     let gender = form.elements["gender"].value;
     let location = form.elements["travel-location"].value;
     let diet = [];
-    if (form.elements['vegan'].checked) {
-        diet.push(document.getElementById('vegan').value);
-    }
-    if (form.elements['gluten'].checked) {
-        diet.push(document.getElementById('gluten').value);
-    }
-    if (form.elements['paleo'].checked) {
-        diet.push(document.getElementById('paleo').value);
+    for(let i = 0; i < form.diet.length; i++) {
+        if(form.diet[i].checked) {
+            diet.push(form.diet[i].value)
+        }
     }
     
     alert(`First Name: ${firstName} \nLast Name: ${lastName} \nAge: ${age} \nGender: ${gender} \nTravel Location: ${location} \nDiet: ${diet} 
-    \nAwesome, now if you die, it won't be an accident.. \n ${confirm(`Prevent this page from creating additional dialogs`)}`);
+    \nAwesome, now if you die, it won't be an accident.. \n Prevent this page from creating additional dialogs`);
     
 }
 
