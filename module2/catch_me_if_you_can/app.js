7f293ef865186c91b7a18ae27d7bbba35cc93e04
Yong Cho <yong-cho@outlook.com>
2021-05-11 00:46:35 -0500

    revised catch me if you can

--- module2/catch_me_if_you_can/app.js
+++ module2/catch_me_if_you_can/app.js
@@ -15,17 +15,19 @@ try {
     sum("1", "2")
 } catch(err) {
     console.log(err)
+} finally {
+    console.log(sum(3, 5))
 }
 //   1b) Call the sum function inside a try block using "1" and "2" as arguments. Use console.log within a catch block to inform the user of the error.
 
-console.log(sum(5, 2))
+
 
 // 2a) Given a user object, write a function called login that takes a username and password as parameters. Throw an error if either of them don't match. Otherwise, log to the console a message saying "login successful!"
 
 var user = {username: "sam", password: "123abc"};
 function login(username, password){
   //check credentials
-  if(username !== 'sam' && password !== '123abc') {
+  if(username !== 'sam' || password !== '123abc') {
       throw 'You have entered incorrect username / password'
   } else {
       throw 'login successful'
@@ -33,13 +35,13 @@ function login(username, password){
 }
 
 try {
-    login("sam", "123abc")
+    login("sam", "123bc")
 } catch(err) {
     console.log(err)
 }
 
 try {
-    login("sa", "12abc")
+    login("sam", "123abc")
 } catch(err) {
     console.log(err)
 }
