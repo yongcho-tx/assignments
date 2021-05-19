45fade02aafee4179f2259462b5a9d9407a1b39e
Yong Cho <yong-cho@outlook.com>
2021-05-19 14:59:16 -0500

    added postman practice

---
+++ module2/postman_practice/app.js
@@ -0,0 +1,48 @@
+// 1. Use Postman to GET a list of planets.
+//https://swapi.dev/api/planets
+
+// 2. Use Postman to GET a single person object.
+
+// https://swapi.dev/api/people/2
+
+// 3. Use Postman to POST 3 new todo objects. Run another GET to see them. Use the following template for your JSON body:
+// {
+//     "title": "This is required",
+//     "description": "This is optional",
+//     "price": 0, //must be a number
+//     "imgUrl": "http://path-to-url.jpg",
+//     "completed": false //must be boolean
+//   }
+
+//     "title": "postman exercise",
+//     "description": "get and post",
+//     "price": 1,
+//     "imgUrl": "http://path-to-url.jpg",
+//     "completed": false
+//   }
+
+
+
+// {
+
+//     "title": "this is my second post",
+//     "description": "second description",
+//     "price": 2,
+//   }
+
+//     "title": "this is my third post",
+//     "description": "third description",
+//     "price": 3,
+//     "imgUrl": "http://path-to-url.jpg",
+//     "completed": false
+//   }
+
+// 4. GET one single todo using its id property.
+// https://api.vschool.io/yong_cho/todo/60a56bb1ee6f8d3fc3fed8fc
+
+// 5. DELETE one single todo using its id property
+// https://api.vschool.io/yong_cho/todo/60a56b3fee6f8d3fc3fed8fa
+
+// 6. PUT one single todo using its id property.
+
+// https://api.vschool.io/yong_cho/todo/60a56b8bee6f8d3fc3fed8fb
