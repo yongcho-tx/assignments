1f887adf9086a7a251e0c7eeefe12518f90eb04c
Yong Cho <yong-cho@outlook.com>
2021-05-20 00:50:47 -0500

    revised pokemon

--- module2/pokemon_list/app.js
+++ module2/pokemon_list/app.js
@@ -1,35 +1,60 @@
 
-const xhr = new XMLHttpRequest()
-        //method   //url                          //async?
-xhr.open("GET", 'https://api.vschool.io/pokemon', true)
-xhr.send()
+// const xhr = new XMLHttpRequest()
+//         //method   //url                          //async?
+// xhr.open("GET", 'https://api.vschool.io/pokemon', true)
+// xhr.send()
 
-// xhr.onload = () => {
-//     console.log(`Data Loaded: ${xhr.status} ${xhr.response}`)
+// // xhr.onload = () => {
+// //     console.log(`Data Loaded: ${xhr.status} ${xhr.response}`)
+// // }
+
+
+
+
+// xhr.onreadystatechange = function() {
+//     if(xhr.readyState === 4 && xhr.status === 200) {
+//         const JSONdata = xhr.responseText
+//         const data = JSON.parse(JSONdata)
+//         console.log(data)
+//         showData(data.objects[0].pokemon)
+//     }
 // }
 
 
+// function showData(arr) {
+//     for(let i = 0; i < arr.length; i++) {
+//         const h1 = document.createElement('h1')
+//         const p = document.createElement('p')
+//         h1.textContent = `Pokemon Name: ${arr[i].name}`
+//         p.textContent = `Detail: ${arr[i].resource_uri}`
+//         document.body.appendChild(h1)
+//         document.body.appendChild(p)
+//     }
+  
+// }
 
+const xhr = new XMLHttpRequest()
 
-xhr.onreadystatechange = function() {
+xhr.open("GET", 'https://api.vschool.io/pokemon', true)
+xhr.send()
+
+xhr.onreadystatechange = () => {
     if(xhr.readyState === 4 && xhr.status === 200) {
-        const JSONdata = xhr.responseText
-        const data = JSON.parse(JSONdata)
-        // console.log(data.pokemon)
-        console.log(data.objects[0].pokemon)
-        showData(data.objects[0].pokemon)
+        const jsonData = xhr.responseText
+        const data = JSON.parse(jsonData)
+        pokeList(data.objects[0].pokemon)
     }
 }
 
-
-function showData(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        const h1 = document.createElement('h1')
-        const p = document.createElement('p')
-        h1.textContent = `Pokemon Name: ${arr[i].name}`
-        p.textContent = `Detail: ${arr[i].resource_uri}`
-        document.body.appendChild(h1)
-        document.body.appendChild(p)
-    }
-  
+const pokeList = (arr) => {
+    arr.forEach(poke => {
+       
+    const h1 = document.createElement('h1')
+    const p = document.createElement('p')
+    h1.textContent = `Pokemon Name: ${poke.name}`
+    p.textContent = `Pokemon Detail: ${poke.resource_uri}`
+    document.body.appendChild(h1)
+    document.body.appendChild(p)
+    
+    })
 }
