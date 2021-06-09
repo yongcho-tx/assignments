ae6c0b375e542b3ca0c408e142cf38e5b912c4ba
Yong Cho <yong-cho@outlook.com>
2021-06-09 09:38:46 -0500

    revised dj-js

--- dj-js/app.js
+++ dj-js/app.js
@@ -1,46 +1,41 @@
+const sq = document.querySelector('.square')
+
 // Blue when the mouse hovers over the square
-const blueBackground = document.getElementById('mouseTarget');
-
-    blueBackground.addEventListener('mouseenter', e => {
-    blueBackground.style.backgroundColor= 'Blue';
-});
-
+    sq.addEventListener('mouseenter', e => sq.style.backgroundColor= 'Blue')
 // Red when the mouse button is held down over the square
-const redBackground = document.getElementById('mouseTarget');
-
-    redBackground.addEventListener('mousedown', e => {
-    redBackground.style.backgroundColor = 'Red';
-    });
-
+    sq.addEventListener('mousedown', e => sq.style.backgroundColor = 'Red')
 // Yellow when the mouse button is released over the square
-const yellowBackground = document.getElementsByClassName('square')[0];
-
-    yellowBackground.addEventListener('mouseup', e => {
-    yellowBackground.style.backgroundColor = 'Yellow';
-    });
-
+    sq.addEventListener('mouseup', e => sq.style.backgroundColor = 'Yellow')
 // Green when double-clicking on the mouse
-const greenBackground = document.querySelector('.square');
-
-    greenBackground.addEventListener('dblclick', e => {
-    greenBackground.style.backgroundColor = 'Green';
-    });
-
+    sq.addEventListener('dblclick', e => sq.style.backgroundColor = 'Green')
+    
 // Orange when scrolling anywhere on the screen
-window.addEventListener("scroll", e => {
-        document.querySelector('.square').style.backgroundColor = 'Orange';
-    })
+window.addEventListener("scroll", e => sq.style.backgroundColor = 'Orange')
 
-document.addEventListener("keydown", e => {
-    if(e.key === 'b' || e.key === 'B') {
-        document.querySelector('.square').style.backgroundColor = "Blue";
-    } if(e.key === 'r' || e.key === 'R') {
-        document.querySelector('.square').style.backgroundColor = "Red";
-    } if(e.key === 'y' || e.key === 'Y') {
-        document.querySelector('.square').style.backgroundColor = "Yellow";
-    } if(e.key === 'g' || e.key === 'G') {
-        document.querySelector('.square').style.backgroundColor = "Green";
-    } if(e.key === 'o' || e.key === 'O') {
-        document.querySelector('.square').style.backgroundColor = "Orange";
-    }
-})
+// document.addEventListener("keydown", e => {
+//       if(e.key === 'b' || e.key === 'B') {
+//         sq.style.backgroundColor = "Blue";
+//     } else if(e.key === 'r' || e.key === 'R') {
+//         sq.style.backgroundColor = "Red";
+//     } else if(e.key === 'y' || e.key === 'Y') {
+//         sq.style.backgroundColor = "Yellow";
+//     } else if(e.key === 'g' || e.key === 'G') {
+//         sq.style.backgroundColor = "Green";
+//     } else if(e.key === 'o' || e.key === 'O') {
+//         sq.style.backgroundColor = "Orange";
+//     }
+// })
+document.addEventListener("keydown", e => (
+        (e.key === 'b' || e.key === 'B') 
+        ? sq.style.backgroundColor = "Blue"
+        :(e.key === 'r' || e.key === 'R') 
+        ? sq.style.backgroundColor = "Red"
+        : (e.key === 'y' || e.key === 'Y') 
+        ? sq.style.backgroundColor = "Yellow"
+        : (e.key === 'g' || e.key === 'G')
+        ? sq.style.backgroundColor = "Green"
+        : (e.key === 'o' || e.key === 'O')
+        ? sq.style.backgroundColor = "Orange"
+        : null
+        )
+)
