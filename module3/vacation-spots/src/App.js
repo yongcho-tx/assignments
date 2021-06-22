ec905d2af90f2e729b0271e86b504ca467f0c354
Yong Cho <yong-cho@outlook.com>
2021-06-22 15:36:21 -0500

    revised vacation-spots

--- module3/vacation-spots/src/App.js
+++ module3/vacation-spots/src/App.js
@@ -1,15 +1,16 @@
 import React from "react"
-import Card from "./Card"
+import Card from "./components/Card"
 import cardData from "./vacationCard"
 import "./styles.css"
 
 
 function App() {
-    const cardComponents = cardData.map(item => <Card key={item.price} card={item}/>)
+    const cardComponents = cardData.map((item, index) => <Card key={item.price} index={index} card={item}/>)
     return (
         <div className="main">
-        <header className="navbar">
-            <h1>Exciting Vacation Spots</h1></header>
+            <header className="navbar">
+                <h1>Exciting Vacation Spots</h1>
+            </header>
             <div className="card-container">
                 {cardComponents}
             </div>
--- module3/vacation-spots/src/components/Card.js
+++ module3/vacation-spots/src/components/Card.js
@@ -1,6 +1,5 @@
 import React from "react"
 
-
 function Card(props) {
     return (
         <div className="card-item" style={{background: props.card.timeToGo === "Summer" ? "red" : props.card.timeToGo === "Spring" ? "#E8F3E8"
@@ -8,13 +7,10 @@ function Card(props) {
             <h2>Destination: {props.card.place}</h2>
             <p>Price: {props.card.price <= 500 ? `$${props.card.price}` : props.card.price > 500 && props.card.price <= 1000 ? `$$${props.card.price}` 
             : `$$$${props.card.price}`}</p>
-            {/* <p style={{background: props.card.timeToGo === "Summer" ? "red" : props.card.timeToGo === "Spring" ? "green"
-            : props.card.timeToGo === "Fall" ? "orange" : "lightblue" }}>Time To Go: {props.card.timeToGo}</p> */}
             <p>Time To Go: {props.card.timeToGo}</p>
-            <img src={props.card.imgUrl}></img>
+            <img src={props.card.imgUrl} alt={props.index}></img>
         </div>
     )
 }
-
 
 export default Card
