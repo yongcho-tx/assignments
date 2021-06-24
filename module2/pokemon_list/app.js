95c0fec684d5042f792233e0c784a2dc7c930424
Yong Cho <yong-cho@outlook.com>
2021-06-24 11:48:50 -0500

    added another solution to poke

--- module2/pokemon_list/app.js
+++ module2/pokemon_list/app.js
@@ -33,28 +33,60 @@
   
 // }
 
+//fetch method
+// fetch("https://api.vschool.io/pokemon")
+//     .then(res => res.json())
+//     .then(res => {
+        
+//         for(let i = 0; i < res.objects[0].pokemon.length; i++){
+//             const h1 = document.createElement('h1')
+//             h1.textContent = res.objects[0].pokemon[i].name
+//             document.body.appendChild(h1)
+//         }
+        
+//     })
+//     .catch(err => console.log(err))
+
+// const xhr = new XMLHttpRequest()
+
+// xhr.open("GET", 'https://api.vschool.io/pokemon', true)
+// xhr.send()
+
+// xhr.onreadystatechange = () => {
+//     if(xhr.readyState === 4 && xhr.status === 200) {
+//         const jsonData = xhr.responseText
+//         const data = JSON.parse(jsonData)
+//         pokeList(data.objects[0].pokemon)
+//     }
+// }
+
+// const pokeList = (arr) => {
+//     arr.forEach(poke => {
+       
+//     const h1 = document.createElement('h1')
+//     const p = document.createElement('p')
+//     h1.textContent = `Pokemon Name: ${poke.name}`
+//     p.textContent = `Pokemon Detail: ${poke.resource_uri}`
+//     document.body.appendChild(h1)
+//     document.body.appendChild(p)
+    
+//     })
+// }
+
+
+
+const poke = document.getElementById('poke')
 const xhr = new XMLHttpRequest()
 
-xhr.open("GET", 'https://api.vschool.io/pokemon', true)
-xhr.send()
-
-xhr.onreadystatechange = () => {
-    if(xhr.readyState === 4 && xhr.status === 200) {
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200){
         const jsonData = xhr.responseText
-        const data = JSON.parse(jsonData)
-        pokeList(data.objects[0].pokemon)
+        const parsed = JSON.parse(jsonData)
+        const pokemon = parsed.objects[0].pokemon
+        pokemon.map(pok => poke.innerHTML += `<h1>Name ${pok.name}</h1> <p>Details ${pok.resource_uri}</p>`)
+        console.log(pokemon)
     }
 }
 
-const pokeList = (arr) => {
-    arr.forEach(poke => {
-       
-    const h1 = document.createElement('h1')
-    const p = document.createElement('p')
-    h1.textContent = `Pokemon Name: ${poke.name}`
-    p.textContent = `Pokemon Detail: ${poke.resource_uri}`
-    document.body.appendChild(h1)
-    document.body.appendChild(p)
-    
-    })
-}
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
